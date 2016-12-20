a4d3621923a75f74f74f789b7edd04eabe70aeed
Paul Florence <florencepaul@hotmail.fr>
2016-12-20 22:43:43 +0100

    Data structure for 3D Objects.

---
+++ render_engine/src/main.rs
@@ -0,0 +1,32 @@
+use std::vec::Vec;
+// Implementation of the Indexed Face Set data structure for representing a mesh
+
+mod math {
+
+    pub struct Vector3<T> {
+        pub x : T,
+        pub y : T,
+        pub z : T,
+    }
+}
+
+mod obj_3d {
+    use math;
+
+    struct Triangle<'a>{
+        vertex : [&'a math::Vector3<f32>; 3]
+    }
+
+    struct Mesh<'a> {
+        vertex_list : Vec<math::Vector3<f32>>,
+        triangle_list : Vec<Triangle<'a>>,
+    }
+}
+
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
