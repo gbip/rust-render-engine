6c174f34fe9af5373fbe55d07e8f8156dde681aa
Paul Florence <florencepaul@hotmail.fr>
2016-12-23 23:09:10 +0100

    Added the implementation of some base traits for abstracting rendering.

--- render_engine/src/main.rs
+++ render_engine/src/main.rs
@@ -3,46 +3,66 @@ use std::vec::Vec;
 mod math;
 
 type Vector3f = math::Vector3<f32>;
-
+/// A tuple that represents a color in a RGB value on 8 bits
+type Color8=(u8,u8,u8);
 mod obj_3d {
     use math;
     struct Triangle<'a>{
         vertex : [&'a math::Vector3<f32>; 3]
     }
     /// The standard Indexed Face Set data structure for mesh.
-    struct Mesh<'a> {
+    pub struct Mesh<'a> {
         vertex_list : Vec<math::Vector3<f32>>,
         triangle_list : Vec<Triangle<'a>>,
     }
 }
 
+mod renderer {
+    use Color8; 
+    use scene;
+    
+    type Image = Vec<Vec<Color8>> ;//[Box<[Color8]>]; 
+    
+    /// Represent a Surface on which you can draw a picture (a screen, a file, etc.)
+    trait Surface {
+        fn write_to(data : &Image);
+    }
+    
+    /// Represent a rendering algorithm
+    trait Render {
+        fn generate_image(world: scene::World) -> Image;
+    }
+}
+
 mod scene {
     use math::{Vector3, VectorialOperations};
+    use Vector3f;
+    use obj_3d;
+    
     pub struct Camera {
    
         /// The position fo the camera exprimed in the standard word space coordinates (where {0,0,0} is the
         /// center of the world)
-        world_position : Vector3<f32>,  
+        world_position : Vector3f,  
        
         /// The position of the point at which the camera is aiming, in world space coordinates
-        target_position : Vector3<f32>, 
+        target_position : Vector3f, 
         
         /// This array of vector is defining a new orthnormal base of the space where :
         ///     - k1 is the vector that describes how the camera is aiming (ie
         /// world_position-target_position).
         ///     - k2 and k3 are choosen randomly.
         base_vector : R3Base, 
-
     }
     
     pub struct R3Base{
-        u: Vector3<f32>, 
-        v: Vector3<f32>,
-        w: Vector3<f32>}
+       u: Vector3f, 
+        v: Vector3f,
+        w: Vector3f}
 
     impl R3Base {
         
-        fn make_camera_base<'a>(&mut self, camera_position : &'a Vector3<f32>, target_position : &'a Vector3<f32>, world_z_axis : &Vector3<f32>) {
+        fn make_camera_base<'a>(&mut self, camera_position : &'a Vector3f, target_position : &'a Vector3f, world_z_axis : &Vector3f) {
             /// Initialisation of the base vectors
             let v1 = camera_position - target_position;// First vector of the new base 
             let k1 = &v1/v1.norm_ref(); // k1 is the normed vector
@@ -61,28 +81,29 @@ mod scene {
         }
     }
 
-    pub struct World {
+    pub struct World<'a> {
         /// The base vector of the world :
         /// the 3rd one is UP (aka we are in XYZ configuration)
         base_vector : [Vector3<f32>; 3],
        
         /// A Vec containing all the cameras in the world
         cameras : Vec<Camera>,
+    
+        objects : Vec<obj_3d::Mesh<'a>>,
     }
 
-    impl World {
-        /// This method create a camera at a given position, 
-        fn add_camera(self : &mut World, position : Vector3<f32>, target : Vector3<f32>) {
+    impl<'a> World<'a> {
+        /// This method create a camera at <position>, aiming at <target>
+        fn add_camera(self : &mut World<'a>, position : Vector3f, target : Vector3f) {
             let mut cam_base = R3Base{u: Vector3::make_vec3(0_f32,0_f32,0_f32),
                                       v: Vector3::make_vec3(0_f32,0_f32,0_f32),
                                       w: Vector3::make_vec3(0_f32,0_f32,0_f32)};
             cam_base.make_camera_base(&position,&target,&self.base_vector[2]);
             let new_cam = Camera{world_position: position,
-                                target_position: target,
-                                base_vector: cam_base};
+                                 target_position: target,
+                                 base_vector: cam_base};
             self.cameras.push(new_cam);
         }
-
     }
 }
 
