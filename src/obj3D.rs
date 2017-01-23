7c606f90e2abd6040299de16ed787fe66e33387b
gbip <florencepaul@hotmail.fr>
2017-01-23 19:20:45 +0100

    Implementation of the obj parser, still a little bit of work to do.

--- src/obj3D.rs
+++ src/obj3D.rs
@@ -3,50 +3,55 @@ use math::{Vector3, Vector3f};
 use std::clone::Clone;
 use render::{Color8};
 
-/// This structs only hold references to the vertex that are stocked in the mesh.
+
 #[derive(Clone)]
 pub struct Triangle {
-    pub vertex: [Vector3<f32>; 3],
+    //Using a vector for easy allocation
+    texture: Vec<Vector3f>,
+    normals: Vec<Vector3f>,
+    pos: Vec<Vector3f>,
 }
 
-pub struct Polygon<'a> {
-    vertex: Vec<&'a Vector3<f32>>,
+impl Triangle {
+    
+    // Basic constructor
+    pub fn new(tex: Vec<Vector3f>, norm: Vec<Vector3f> , pos:Vec<Vector3f>) -> Triangle {
+        Triangle{texture: tex,
+                normals: norm,
+                pos: pos}
+    }
+
 }
 
 /// The standard Indexed Face Set data structure for mesh.
 pub struct Mesh {
-    triangle_list: Vec<Triangle>,
+
+    triangles : Vec<Triangle>,
 }
 
 impl Mesh {
-
-    fn new(vertex: Vec<Vector3f>,face_indices: Vec<(usize,usize,usize)>) -> Mesh {
-        let mut mesh_triangles : Vec<Triangle> = vec!();
-        for triangle in face_indices {
-            let (v1,v2,v3) = triangle;
-            mesh_triangles.push(Triangle{vertex:[vertex[v1],vertex[v2],vertex[v3]]});
-        }
-        Mesh{triangle_list:mesh_triangles}
-    }
-    fn new_from_obj(path:String) -> Mesh {
-        unimplemented!()/*
-        let (vertex, face_indices) = obj::generate_vec_from_obj(path);
-        Mesh::new(vertex,face_indices)*/
-
-    }
-    fn new_empty() -> Mesh {
-        Mesh{triangle_list:vec!()}
+    
+   
+    pub fn add_triangle(&mut self, tri : Triangle) {
+        self.triangles.push(tri);
     }
 
-    /// A function for Serde, do not rename because it will break Serde.
-    fn empty() -> Mesh {
-        Mesh::new_empty()
+    // Creates a new empty mesh
+    pub fn new_empty() -> Mesh {
+        Mesh{triangles: vec!()}
     }
+
+    // Returns the triangle-ith triangle of the mesh (where triangle is it's coordinate in the
+    // vector. Clone the value
+    pub fn get_triangle(&self, triangle : usize) -> Triangle {
+        self.triangles[triangle].clone()    
+    }
+
 }
 
 #[derive(Serialize,Deserialize)]
 pub struct Object {
-    #[serde(skip_serializing,skip_deserializing,default = "Mesh::empty")]    
+    #[serde(skip_serializing,skip_deserializing,default = "Mesh::new_empty")]    
     ///The internal geometry data
     mesh: Mesh,
     
@@ -60,17 +65,6 @@ pub struct Object {
     obj_path: String,    
 }
 
-impl Object {
-    pub fn get_triangles(&self) -> Vec<Triangle> {
-
-        let mut result: Vec<Triangle> = vec![];
-        for triangles in &self.mesh.triangle_list {
-            result.push(triangles.clone());
-        }
-        return result;
-    }
-}
-
 mod obj_parser {
     use std::fs::File;
     use super::Mesh;
@@ -79,62 +73,63 @@ mod obj_parser {
     enum LineType {
         Ignore,
         Face(u32,u32,u32),
-        Vertex(f32,f32,f32)
+        Vertex(f32,f32,f32),
+        Normal(f32,f32,f32),
+        TexCoord(f32,f32),
     }
 
-    fn get_obj_data_from_file(path: &str) -> Vec<(f32,f32,f32)> {
-        
-       unimplemented!(); 
-    let objects = split_into_objects(open_obj_file(path));   
-        
-        
-        
+    //Split a given line and parse each float value inside.
+    fn get_floats(line : String) -> Vec<f32> {
+        ///TODO FIX THE BROKEC EXPECT §§§
+        line.split_whitespace().map(|val : &str| (val).parse::<f32>().expect("Invalid float value.")).collect()
     }
     
-    
-    
+    fn parse_normal(line : String) -> Result<LineType,String> {
+        let floats = get_floats(line);
+        if floats.len() == 3 {
+            Ok(LineType::Normal(floats[0],floats[1],floats[2]))
+        }
+        else {
+            Err(format!("Invalide number of float value : {} ",floats.len()))
+        }
+    }
+
+    fn parse_vertex(line : String) -> Result<LineType,String> {
+        match parse_normal(line) {
+            Ok(LineType::Normal(u,v,w)) => Ok(LineType::Vertex(u,v,w)),
+            Err(t) => Err(t),
+            _ => unreachable!()
+        }
+    }
+
+    fn parse_face(line: String ) -> Result<LineType,String> {
+        unimplemented!()
+    }
+
+    fn parse_tex_coord(line: String) -> Result<LineType,String> {
+        unimplemented!()
+    }
+
+
+    fn parse_line(line : String) -> Result<LineType,String> {
+        match line.chars().nth(0).expect("Error while reading line") {
+            //Trivial cas, we just doesn't support groups, or individual objects
+            'o' | 'g' |'#' | 'u' | 'm' | 's' => Ok(LineType::Ignore),
+            'f' => parse_face(line),
+            'v' => match line.chars().nth(1).expect("Error while reading line") {
+                ' ' => parse_vertex(line),
+                'n' => parse_normal(line),
+                't' => parse_tex_coord(line),
+                _ => Err("Unexpected symbol".to_string()), 
+            },
+            _ => Err("Unexpected symbol".to_string()),
+        }
+    }
+
     fn open_obj_file(path: &str) -> File {
         match File::open(path) {
             Ok(t) => t,
             Err(e) => panic!("Error while trying to open the file: {} - {}", path,e),
         }
     }
-    
-    fn split_into_objects(file: File) -> Vec<Vec<String>> {
-        let buffer = BufReader::new(&file);
-        let mut result : Vec<Vec<String>> = vec!(vec!());
-        let mut is_parsing = false;
-        let mut temp_object: Vec<String> = vec!();
-        // with the map and the Filter we insure 2 things : we unwrap the result of the BufReader
-        // which can fail at reading a file and we make sure that each lines isn't empty so that
-        // line.chars().nth(0).unwrap() can be called safely
-        for line in buffer.lines().map(|r| r.expect("Fatal error while reading the .obj file")).filter(|s| s.len()>0) {
-            if line.chars().nth(0).unwrap() == 'o' {
-                if is_parsing {
-                    result.push(temp_object);
-                    temp_object = vec!();
-                    continue;
-                }
-                else {
-                    is_parsing = true;
-                    continue;
-                }
-            }
-            else {
-                if is_parsing {
-                    temp_object.push(line);
-                }
-                else {
-                    continue;
-                }
-            }
-        }
-        result
-    }
-
-    pub fn parse_obj(path : &str) -> super::Mesh {
-        open_obj_file(path);
-        Mesh::new_empty()
-    }
-}
-
+} 
